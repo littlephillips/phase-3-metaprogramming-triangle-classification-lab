42ad8620c53dad27e4691145fb63920a95bfc47c
littlephillips <phillipkinuthia72@gmail.com>
2022-10-23 15:05:32 +0300

    triangle classification

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+
+  def initialize(length, height, width)
+    @length = length
+    @height =  height
+    @width =  width
+  end
+
+  def kind
+    if (@length <= 0 || @height <= 0 || @width <= 0)
+      raise TriangleError
+    elsif(@length + @height <= @width|| @height + @width <= @length || @length + @width <= @height)
+      raise TriangleError
+    elsif (@length == @height && @height == @width)  
+      :equilateral
+    elsif (@length == @height || @height == @width || @length == @width)  
+      :isosceles 
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
